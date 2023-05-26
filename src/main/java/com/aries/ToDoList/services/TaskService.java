8f05872dbf54391775d4246a3e1bd7f5b533718f
Aries023 <pet.potocka@gmail.com>
2023-05-27 00:28:51 +0200

    created services (folder) created interface class TaskService into it and started writhe methods but i will continue tomorrow.

---
+++ src/main/java/com/aries/ToDoList/services/TaskService.java
@@ -0,0 +1,11 @@
+package com.aries.ToDoList.services;
+
+import com.aries.ToDoList.domain.Task;
+
+import java.util.List;
+
+public interface TaskService {
+    List<Task> getTasks();
+    Task get (Integer id);
+    List<Task> getTaskByUserId(Integer id);
+}

3889af685c1d93da7e35dfcf967b60eea85c3bf2
Aries023 <pet.potocka@gmail.com>
2023-05-31 22:05:22 +0200

    completion interface TasServices. added methods add, delete, update.

--- src/main/java/com/aries/ToDoList/services/TaskService.java
+++ src/main/java/com/aries/ToDoList/services/TaskService.java
@@ -7,5 +7,8 @@ import java.util.List;
 public interface TaskService {
     List<Task> getTasks();
     Task get (Integer id);
-    List<Task> getTaskByUserId(Integer id);
+    List<Task> getTaskByUserId(Integer userId);
+    Integer add(Task task);
+    void delete (Integer id);
+    void update(Integer id , Task task);
 }
